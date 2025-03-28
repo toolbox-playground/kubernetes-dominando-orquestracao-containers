c83694795f11079dcac59d0daeb96cad79f271be
thihenos <thihenos@gmail.com>
2025-03-28 19:45:59 -0300

    feat: codigo para hpa

--- conteudo/05_modulo/05_02-k6/load_test.js
+++ conteudo/05_modulo/05_02-k6/load_test.js
@@ -3,10 +3,10 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '10s', target: 10 }, // Aumenta para 10 usuários em 10s
-    { duration: '30s', target: 50 }, // Mantém 50 usuários por 30s
-    { duration: '10s', target: 2500 }, // Mantém 50 usuários por 30s
-    { duration: '10s', target: 2500 }, // Mantém 50 usuários por 30s
+    { duration: '10', target: 1000 }, // Aumenta para 10 usuários em 10s
+    { duration: '10s', target: 5000 }, // Mantém 50 usuários por 30s
+    { duration: '10s', target: 25000 }, // Mantém 50 usuários por 30s
+    { duration: '10s', target: 250000 }, // Mantém 50 usuários por 30s
     { duration: '10s', target: 0 },  // Reduz para 0 usuários em 10s
   ],
 };
@@ -15,8 +15,7 @@ export default function () {
   let res = http.get('http://localhost:3000/home');
 
   check(res, {
-    'status é 200': (r) => r.status === 200,
-    'resposta contém "Test!"': (r) => r.body.includes("Test!"),
+    'status é 200': (r) => r.status === 200
   });
 
   //let res = http.get('http://localhost:30003/test');
